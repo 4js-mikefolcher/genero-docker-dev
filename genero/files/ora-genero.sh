1b094465713b761af14dc153e0c1da51405c47b0
Mike Folcher <mifo@4js.com>
2024-02-29 12:02:16 -0500

    Create .wallet directory
    Create the .wallet directory as the first command in ora-genero.sh

--- genero/files/ora-genero.sh
+++ genero/files/ora-genero.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#Command 1: Create the wallet
+#Command 1: Create the wallet directory
+mkdir /opt/oracle_client/.wallet
+
+#Command 2: Create the wallet
 mkstore -wrl /opt/oracle_client/.wallet -create
 
-#Command 2: Add the Genero account and password to the wallet
+#Command 3: Add the Genero account and password to the wallet
 mkstore -wrl /opt/oracle_client/.wallet -createCredential generodb genero Pass123
